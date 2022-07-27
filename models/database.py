05ea57d0d0152b976df6e1f1e72ddeb9c48bbf42
yuinakorn <yuinakorn@hotmail.com>
2022-07-28 00:44:18 +0700

    update database config

--- models/database.py
+++ models/database.py
@@ -8,14 +8,7 @@ from decouple import config
 DATABASE_URL = config('DATABASE_URL')
 MYSQL_PASSWORD = config('MYSQL_PASSWORD')
 
-engine = create_engine(DATABASE_URL % quote(MYSQL_PASSWORD),
-                       connect_args={"connect_timeout": 10},
-                       pool_size=20,
-                       max_overflow=30,
-                       pool_recycle=3600,
-                       pool_pre_ping=True,
-                       pool_timeout=10)
-
+engine = create_engine(DATABASE_URL % quote(MYSQL_PASSWORD))
 session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
